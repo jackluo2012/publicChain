d9c364c66a7fedf5618e4c1a577038a176770888
jackluo <net.webjoy@gmail.com>
2019-09-06 15:56:55 +0800

    test

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/labstack/gommon/log"
 	"publicChain/BLC"
 )
 
@@ -29,4 +31,39 @@ func main() {
 	fmt.Println(bytess)
 	block = BLC.DeserializeBlock(bytess)
 	fmt.Printf("%d\n", block.Nonce)
+
+	//
+	boltTest()
+}
+
+func boltTest() {
+	db, err := bolt.Open("my.db", 06000, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+
+	//创建表 .
+	err = db.Update(func(tx *bolt.Tx) error {
+
+		//创建BlockBucket表
+		b, err := tx.CreateBucket([]byte("BlockBucket"))
+		if err != nil {
+			return fmt.Errorf("Create bucket: %s", err)
+		}
+		//往表里面存储数据
+		if b != nil {
+			b.Put([]byte("l"), []byte("Send 100 BTC To 强哥......"))
+			if err != nil {
+				log.Panic("数据存储失败")
+			}
+		}
+
+		return nil
+	})
+
+	// 更新失败
+	if err != nil {
+		log.Panic(err)
+	}
 }
